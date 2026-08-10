4d6ed6481dc3b32465a38a1cad67f3678a033466
arimakridakis <ari.liminus@gmail.com>
2026-08-10 16:41:59 -0400

    Add welcome email templates for registrations

---
+++ api/registration-emails/base-camp-tuesday.ts
@@ -0,0 +1,9 @@
+export const baseCampTuesdayWelcome = {
+  subject: 'Welcome to Fall 2026 Base Camp — Tuesday',
+  text: `Dear {{firstName}},
+
+Thank you for registering for Fall 2026 Base Camp — Tuesday. We are delighted that you'll be joining us this fall.
+
+Warmly,
+The Wayfinders team`,
+};
---
+++ api/registration-emails/holiday-camp-indigenous-peoples-day.ts
@@ -0,0 +1,9 @@
+export const holidayCampIndigenousPeoplesDayWelcome = {
+  subject: 'Welcome to Holiday Camp — Indigenous Peoples’ Day',
+  text: `Dear {{firstName}},
+
+Thank you for registering for Holiday Camp — Indigenous Peoples’ Day. We are delighted that you'll be joining us this fall.
+
+Warmly,
+The Wayfinders team`,
+};
